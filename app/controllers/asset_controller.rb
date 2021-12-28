a63dd4a14702d697a8f15693050ce3f2fb0c450a
jd2001 <joshuadeppe15@gmail.com>
2021-12-28 16:03:34 +0000

    added asset controller

--- app/controllers/asset_controller.rb
+++ app/controllers/asset_controller.rb
@@ -1,3 +1,11 @@
 class AssetController < ApplicationController
+  def index
+    @assets = Asset.all
+  end
 
+  private
+
+  def asset_params
+    params.require(:asset).permit(:asset_code, :cost, :cost_type, :name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'asset#index'
+
+  resource :assets
 
   devise_for :users
 end
