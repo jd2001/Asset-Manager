f43acf9a15648744a78902dae282cc667754ba33
jd2001 <joshuadeppe15@gmail.com>
2022-01-12 15:14:37 +0000

    added new colours to tailwind and prototyped root page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
