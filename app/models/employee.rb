6ac24320b947080670782f7bd3c90809788e9f2c
jd2001 <joshuadeppe15@gmail.com>
2022-04-22 12:12:22 +0100

    added navbar access to all indices

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,8 @@ class Employee < ApplicationRecord
   belongs_to :company
   has_many :accesses
   has_many :business_assets, through: :accesses
+
+  def full_name
+    first_name + ' ' + surname
+  end
 end
